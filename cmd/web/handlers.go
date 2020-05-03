54886bba774dca55f85228b0e4a8dbe72c7e9bcb
Cristiano Pacheco <chris.spb25@gmail.com>
2020-05-03 13:53:05 -0300

    Added list endpoints

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -9,4 +14,28 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte("Oh Yeah!"))
+}
+
+// lists
+func (app *application) listIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("List Index"))
+}
+
+func (app *application) listStore(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("List Store"))
+}
+
+func (app *application) listShow(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	fmt.Fprintf(w, "List Show %s", id)
+}
+
+func (app *application) listUpdate(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	fmt.Fprintf(w, "List Update %s", id)
+}
+
+func (app *application) listDestroy(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	fmt.Fprintf(w, "List Destroy %s", id)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,5 +7,12 @@ import (
 func (app *application) routes() *chi.Mux {
 	router := chi.NewRouter()
 	router.Get("/", app.home)
+
+	router.Get("/lists", app.listIndex)
+	router.Post("/lists", app.listStore)
+	router.Get("/lists/{id}", app.listShow)
+	router.Put("/lists/{id}", app.listUpdate)
+	router.Delete("/lists/{id}", app.listDestroy)
+
 	return router
 }
