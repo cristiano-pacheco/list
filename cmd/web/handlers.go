dc903d3d9f38f0f59ea7f26efed09971bfb97859
Cristiano Pacheco <chris.spb25@gmail.com>
2020-05-03 21:57:59 -0300

    Added features to update and delete lists

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
 	"strconv"
 
@@ -21,11 +21,44 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 // lists
 func (app *application) listIndex(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("List Index"))
+	lists, err := app.lists.GetAll()
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.respondwithJSON(w, 200, lists)
 }
 
 func (app *application) listStore(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("List Store"))
+	list := &models.List{}
+	json.NewDecoder(r.Body).Decode(&list)
+
+	listID, err := app.lists.Insert(list.Name)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	if listID <= 0 {
+		app.serverError(w, models.IsNotPossibleToCreate)
+		return
+	}
+
+	list, err = app.lists.Get(listID)
+
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	app.respondwithJSON(w, http.StatusCreated, list)
 }
 
 func (app *application) listShow(w http.ResponseWriter, r *http.Request) {
@@ -47,15 +80,65 @@ func (app *application) listShow(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.respondwithJSON(w, 200, list)
+	app.respondwithJSON(w, http.StatusOK, list)
 }
 
 func (app *application) listUpdate(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	fmt.Fprintf(w, "List Update %s", id)
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	list := &models.List{}
+	json.NewDecoder(r.Body).Decode(&list)
+
+	result, err := app.lists.Update(id, list.Name)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	if result != true {
+		app.serverError(w, models.IsNotPossibleToUpdate)
+		return
+	}
+
+	list, err = app.lists.Get(id)
+
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	app.respondwithJSON(w, http.StatusOK, list)
 }
 
 func (app *application) listDestroy(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
-	fmt.Fprintf(w, "List Destroy %s", id)
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	result, err := app.lists.Delete(id)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	if result != true {
+		app.serverError(w, models.IsNotPossibleToDelete)
+		return
+	}
+
+	app.respondwithJSON(w, http.StatusNoContent, nil)
 }
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -6,6 +6,9 @@ import (
 )
 
 var ErrNoRecord = errors.New("models: no matching record found")
+var IsNotPossibleToDelete = errors.New("models: is not possible to delete the resource")
+var IsNotPossibleToCreate = errors.New("models: is not possible to create the resource")
+var IsNotPossibleToUpdate = errors.New("models: is not possible to update the resource")
 
 type List struct {
 	ID        int       `json:"id"`
